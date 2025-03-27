282c575829dc5acbeb9362b4c0d4f73c0cc14c6f
Muthu Kumar <muthukumar@thefeathers.in>
2025-03-27 22:57:28 +0530

    chore: add jsr.json

--- version.ts
+++ version.ts
@@ -7,7 +7,11 @@ if (!version)
 	throw new Error("VERSION environment variable not found. Use similar to `VERSION=0.1.0 bun run version.ts`");
 
 const pkg = JSON.parse(await Bun.file("./package.json").text());
-
 pkg.version = version;
 
 await Bun.write("./package.json", JSON.stringify(pkg, null, "\t") + "\n");
+
+const jsr = JSON.parse(await Bun.file("./jsr.json").text());
+jsr.version = version;
+
+await Bun.write("./jsr.json", JSON.stringify(jsr, null, "\t") + "\n");
