095d22a1aeb5a43725ac7cf48dc4c61a45c34293
Muthu Kumar <muthukumar@thefeathers.in>
2025-03-27 20:11:09 +0530

    chore: add version.ts and master will always be 0.0.0-dev

---
+++ version.ts
@@ -0,0 +1,13 @@
+export {};
+
+const tag = process.env.GITHUB_REF;
+const version = tag ? tag.slice(11) : process.env.VERSION;
+
+if (!version)
+	throw new Error("VERSION environment variable not found. Use similar to `VERSION=0.1.0 bun run version.ts`");
+
+const pkg = JSON.parse(await Bun.file("./package.json").text());
+
+pkg.version = version;
+
+await Bun.write("./package.json", JSON.stringify(pkg, null, 2));
